db8f7c35564f3e4f61203b71b2d938b1d3860fa5
zziminking <ckwkek@gmail.com>
2023-04-12 00:00:47 +0900

    컨트롤러 Api

---
+++ hello-spring/src/main/java/springbegin/hellospring/domain/Member.java
@@ -0,0 +1,13 @@
+package springbegin.hellospring.domain;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Member {
+    private long id;
+    private String name;
+
+}
---
+++ hello-spring/src/main/java/springbegin/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,34 @@
+package springbegin.hellospring.repository;
+
+import springbegin.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+    private static Map<Long, Member> storeMap = new HashMap<>();
+    private static Long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        storeMap.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(storeMap.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return storeMap.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(storeMap.values());
+    }
+}
